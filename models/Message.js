f4db9177cf9588d2cebd8faf7ca9d4d044ed5c32
MrYangYang <lys701@gmail.com>
2013-10-21 23:38:05 +0800

    Create socketdev branch
    1.Add BufferPacker.js to pack and unpack socket buffer
    2.Add RedisDB.js to provide redis client obj.
    3.Add ConnectionManager.js to manager connections and binding userid
      with socket.
    4.Add Message model.
    5.Modify package.json to add head_body_buffers package.
    
    TODO
      1.complete unpack function of BufferPacker
      2.complete forward, save, getUnreadByUserID function of Message.js
    	new file:   lib/BufferPacker.js
    	new file:   lib/ConnectionManager.js
    	new file:   lib/RedisDB.js
    	new file:   models/Message.js
    	modified:   package.json

---
+++ models/Message.js
@@ -0,0 +1,35 @@
+var manager = require('../lib/ConnectionManager.js');
+
+function Message (fromID, toID, sendTime, msgContent) {
+	this.fromID     = fromID;
+	this.toID       = toID;
+	this.sendTime   = sendTime;
+	this.msgContent = msgContent;
+};
+
+/**
+ * forward a message to message.toID
+ * @param
+ * @return true if susscess, false otherwise
+ */
+Message.prototype.forward = function(callback) {
+	// body...
+};
+
+/**
+ * save message obj to redis
+ * @param callback
+ * @return 
+ */
+Message.prototype.save = function(callback) {
+	// body...
+};
+
+/**
+ * get unread message of user.
+ * @param userid, callback
+ * @ return 
+ */
+Message.prototype.getUnreadByUserID = function(userID, callback) {
+	// body...
+};
