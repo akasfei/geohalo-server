ef1b1da2aee3c38ec568348ca0e2a739fce84345
MrYangYang <lys701@gmail.com>
2013-10-22 13:45:49 +0800

    fix code indent 	modified:   lib/BufferPacker.js 	modified:   lib/ConnectionManager.js 	modified:   models/Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,11 @@
 var manager = require('../lib/ConnectionManager.js');
+var redis_cli = require('./lib/RedisDB.js').client;
 
 function Message (fromID, toID, sendTime, msgContent) {
-	this.fromID     = fromID;
-	this.toID       = toID;
-	this.sendTime   = sendTime;
-	this.msgContent = msgContent;
+  this.fromID     = fromID;
+  this.toID       = toID;
+  this.sendTime   = sendTime;
+  this.msgContent = msgContent;
 };
 
 /**
@@ -13,16 +14,16 @@ function Message (fromID, toID, sendTime, msgContent) {
  * @return true if susscess, false otherwise
  */
 Message.prototype.forward = function(callback) {
-	// body...
+  // body...
 };
 
 /**
  * save message obj to redis
  * @param callback
  * @return 
- */
+ */ 
 Message.prototype.save = function(callback) {
-	// body...
+  // body...
 };
 
 /**
@@ -31,5 +32,5 @@ Message.prototype.save = function(callback) {
  * @ return 
  */
 Message.prototype.getUnreadByUserID = function(userID, callback) {
-	// body...
+  // body...
 };
